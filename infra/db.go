503acc282275a6b29c64f01abc25b614ea40decb
山﨑 貴生 <yamasaki-ta@MACN0049.local>
2022-11-25 11:07:06 +0900

    DB処理の実装

--- infra/db.go
+++ infra/db.go
@@ -6,7 +6,7 @@ import (
 	"todo_gorm/domain"
 )
 
-// DB初期化
+// DBの初期化
 func dbInit() *gorm.DB {
 	db, err := gorm.Open("mysql", "root:@tcp(localhost:3306)/todo_gorm?parseTime=true")
 	if err != nil {
@@ -14,4 +14,50 @@ func dbInit() *gorm.DB {
 	}
 	db.AutoMigrate(&domain.Todo{})
 	return db
+}
+
+// DBの作成処理
+func dbCreate(todo domain.Todo) {
+	db, err := gorm.Open("mysql", "root:@tcp(localhost:3306)/todo_gorm?parseTime=true")
+	if err != nil {
+		fmt.Errorf("cannot open database")
+	}
+	db.Create(&todo)
+} 
+
+//DBの読込処理
+func dbRead(id ...int) []domain.Todo {
+	db, err := gorm.Open("mysql", "root:@tcp(localhost:3306)/todo_gorm?parseTime=true")
+	if err != nil {
+		fmt.Errorf("cannot open database")
+	}
+	var todos []domain.Todo
+	db.Find(&todos)
+	return todos
+}
+
+//DBの更新処理
+func dbUpdate(id int, text string, status domain.Status, deadline int) domain.Todo {
+	db, err := gorm.Open("mysql", "root:@tcp(localhost:3306)/todo_gorm?parseTime=true")
+	if err != nil {
+		fmt.Errorf("cannot open database")
+	}
+	var todo domain.Todo
+	db.First(&todo, id)
+	todo.Text = text
+	todo.Status = status
+	todo.Deadline = deadline
+	db.Save(&todo)
+	return todo
+} 
+
+//DBの削除処理
+func dbDelete(id int) {
+	db, err := gorm.Open("mysql", "root:@tcp(localhost:3306)/todo_gorm?parseTime=true")
+	if err != nil {
+		fmt.Errorf("cannot open database")
+	}
+	var todo domain.Todo
+	db.First(&todo, id)
+	db.Delete(&todo)
 }
