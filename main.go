e3edb3d9cf85470e833b866b6172bb49759e53f6
山﨑 貴生 <yamasaki-ta@MACN0049.local>
2022-11-25 11:35:18 +0900

    POST処理の実装

--- main.go
+++ main.go
@@ -1,18 +1,48 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
+	"todo_gorm/infra"
+	"todo_gorm/domain"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	r.LoadHTMLGlob("templates/*")
-	r.GET("/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title":    "main website",
-			"contents": "main contents",
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
+	
+	db := infra.DBInit()
+	defer db.Close()
+	
+	router.GET("/", func(c *gin.Context) {
+		todos := infra.DBRead()
+		c.HTML(200, "index.html", gin.H{
+			"todos": todos,
 		})
 	})
-	r.Run(":8080")
+	
+	//POSTの処理
+	router.POST("/new", func(c *gin.Context) {
+		text := c.PostForm("text")
+		rawStatus := c.PostForm("status")
+		id, err := strconv.Atoi(rawStatus)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+		status := domain.Status(id)
+		rawTime := c.PostForm("deadline")
+		deadline, err := strconv.Atoi(rawTime)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+		todo := domain.Todo{Text: text, Status: status, Deadline: deadline}
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+		infra.DBCreate(todo)
+		c.Redirect(302, "/")
+	})
 }
